2bfe9d87a469fccdfe9633903a2c9d01989d474b
michaelBezick <mbezick@purdue.edu>
2023-11-15 19:30:55 -0500

    Changes

---
+++ jump.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <setjmp.h>
+#include <signal.h>
+
+jmp_buf env;
+
+void SIGHandler(int pid) {
+  printf("Bro what was that\n");
+  longjmp(env, 1);
+}
+
+int main(void) {
+
+  signal(SIGFPE, SIGHandler);
+
+  int i = setjmp(env);
+  if (i == 1) {
+    exit(0);
+  }
+  else {
+    int temp = 1 / 0;
+  }
+
+  return 0;
+}
